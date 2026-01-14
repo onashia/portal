51ab5b0e02e5a02387352918f3a87c8191e76353
onashia <74217181+onashia@users.noreply.github.com>
2026-01-14 10:07:46 -0800

    refactor(constants): change mutable ints to const for better immutability

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,8 +1,8 @@
 class AppConstants {
   static const double defaultSpacing = 24.0;
   static const int maxStackedAvatars = 5;
-  static final int maxBackoffDelay = 300;
-  static final int pollingIntervalSeconds = 60;
+  static const int maxBackoffDelay = 300;
+  static const int pollingIntervalSeconds = 60;
   static const int defaultPollingInterval = 60;
   static const int maxAvatarCacheSize = 100;
 }
