aab5c26f9c0e0587cd2b0b36a9858ab8c0ca97ae
onashia <74217181+onashia@users.noreply.github.com>
2026-01-14 11:36:04 -0800

    fix(auth): update vrchat api contact email address

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -161,7 +161,7 @@ final vrchatApiProvider = Provider<VrchatDart>((ref) {
     userAgent: VrchatUserAgent(
       applicationName: 'Portal',
       version: '1.0.0',
-      contactInfo: 'support@portal.app',
+      contactInfo: 'portal@onashia.com',
     ),
   );
 });
